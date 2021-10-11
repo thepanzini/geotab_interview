27a98f2e27380415d1ce82878172f5b1c9bbe077
Julian P <thepanzini@gmail.com>
2021-10-11 16:57:07 -0400

    feat: add some output formatting and more test cases

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ def format_number(value):
     """
     # Some good sanity checks
     # Check for string
-    if value is None or isinstance(value, str):
-        return 0
+    # Check for negative
+    # Check for 0
+    if value is None or isinstance(value, str) or value < 0:
+        return formatted_error(value)
     # Less then 1M return number (999,999 and down)
     if value < 1000000:
-        return value
+        return formatted_output(value, value)
 
     suffix = ""
 
@@ -35,28 +37,84 @@ def format_number(value):
         # No decimal needed (1B)
         output = f"{val_str[0]}{suffix}"
 
-    return output
+    return formatted_output(value, output)
+
+
+def formatted_output(value, output):
+    """Returns a nicely formatted string describing the format change.
+        :param value: The number input
+        :type value: float
+        :param output: The format output
+        :type value: float
+        :returns: A string containing the input transformation and the final output
+        :rtype str
+        """
+    return f"Input Value: {value}\nOutput Format: {output}"
+
+
+def formatted_error(value):
+    """Returns a nicely formatted error message for why the formatting could not happen.
+        :param value: The number input
+        :type value: any
+        :returns: A string containing the inputted number and error info
+        :rtype str
+        """
+    check = type_check(value)
+    return f"Input Value: '{value}' of type {check} is not an valid input and can not be formatted"
+
+
+def type_check(value):
+    """Returns a nice format of variable type.
+        :param value: The value to check type of
+        :returns: A string containing the type
+        :rtype str
+        """
+    check = type(value)
+    if check is str:
+        return 'str'
+    elif check is int:
+        if value < 0:
+            return 'int(negative)'
+        return 'int'
+    elif check is float:
+        return 'int'
+    else:
+        return check
 
 
 if __name__ == '__main__':
+    print("TESTS:")
     # Provided Test Cases:
     # input: 1000000
     # output: 1M
+    print(format_number(1000000))
     # input: 2500000.34
     # output: 2.5M
+    print(format_number(2500000.34))
     # input: 532
     # output: 532
+    print(format_number(532))
     # input: 1123456789
     # output: 1.1B
+    print(format_number(1123456789))
 
-    # My own test cases:
+    print("\nEXTRA TESTS:")
+    # My additional Test Cases:
     # input: "1123456789"
     # output: 0
+    print(format_number("1123456789"))
+    # input: 1
+    # output: 1
+    print(format_number(1))
     # input: 0
     # output: 0
-    print(format_number(1000000))
-    print(format_number(2500000.34))
-    print(format_number(532))
-    print(format_number(1123456789))
-    print(format_number("1123456789"))
-    print(format_number(1))
+    print(format_number(0))
+    # input: -1000000
+    # output: 0
+    print(format_number(-1000000))
+    # input: 9999999999999
+    # output: 9.9T
+    print(format_number(9999999999999))
+    # input: 90000000000000
+    # output: 9T
+    print(format_number(90000000000000))
